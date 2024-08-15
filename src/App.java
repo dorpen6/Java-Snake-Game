a2b510938cfc339b12100bb683c9be80c88001fb
dorpen6 <alivalishov20@gmail.com>
2024-08-15 17:26:39 +0400

    Create SnakeGame Class and Configure JPanel

--- src/App.java
+++ src/App.java
@@ -13,5 +13,9 @@ public class App {
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
+        frame.add(snakeGame);
+        frame.pack(); // Place the JPanel inside the Window (Perfectly)
     }
 }
