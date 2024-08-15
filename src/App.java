f1ec166afc3dd7cda779961c50828f5b5386b7db
dorpen6 <alivalishov20@gmail.com>
2024-08-16 01:58:13 +0400

    Make Snake Move, listen to Key Inputs

--- src/App.java
+++ src/App.java
@@ -19,6 +19,6 @@ public class App {
         SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
         frame.add(snakeGame);
         frame.pack(); // Place the JPanel inside the Window (Perfectly)
-
+        snakeGame.requestFocus();
     }
 }
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -3,11 +3,11 @@ import java.awt.event.*;
 import java.util.Random;
 import javax.swing.*;
 
-public class SnakeGame extends JPanel implements ActionListener {
+public class SnakeGame extends JPanel implements ActionListener, KeyListener {
     private class Tile {
         int x;
         int y;
-
+        
         Tile(int x, int y) {
             this.x = x;
             this.y = y;
@@ -27,12 +27,16 @@ public class SnakeGame extends JPanel implements ActionListener {
 
     // Game Logic
     Timer gameLoop;
+    int velocityX;
+    int velocityY;
 
     SnakeGame(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
         setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
         setBackground(Color.black);
+        addKeyListener(this);
+        setFocusable(true);
 
         snakeHead = new Tile(5, 5);
 
@@ -40,8 +44,15 @@ public class SnakeGame extends JPanel implements ActionListener {
         random = new Random();
         placeFood();
         
+        velocityX = 0;
+        velocityY = 0;
+
         gameLoop = new Timer(100, this);
         gameLoop.start(); // Start the game loop
+
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
     }
 
     public void paintComponent(Graphics g) {
@@ -70,8 +81,47 @@ public class SnakeGame extends JPanel implements ActionListener {
         food.y = random.nextInt(boardHeight / tileSize); // 600/25 = 24 (0:24)
     }
 
+    public void move() {
+        // Snake Head
+        snakeHead.x += velocityX;
+        snakeHead.y += velocityY;
+
+        // Handle wall collision (wrap-around effect)
+        if (snakeHead.x < 0) snakeHead.x = boardWidth / tileSize - 1;
+        if (snakeHead.x >= boardWidth / tileSize) snakeHead.x = 0;
+        if (snakeHead.y < 0) snakeHead.y = boardHeight / tileSize - 1;
+        if (snakeHead.y >= boardHeight / tileSize) snakeHead.y = 0;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+        move();
         repaint();
     }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            velocityX = 0;
+            velocityY = -1;
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            velocityX = 0;
+            velocityY = 1;
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            velocityX = -1;
+            velocityY = 0;
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            velocityX = 1;
+            velocityY = 0;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
 }
