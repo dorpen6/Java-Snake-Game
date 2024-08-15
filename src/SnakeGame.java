754149a919d182e3c6c1c8e8cc4d47293d484687
dorpen6 <alivalishov20@gmail.com>
2024-08-16 02:17:59 +0400

    make Snake Body parts follow the player

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -102,6 +102,20 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
             placeFood();
         }
 
+        // Snake Body
+        for (int i = snakeBody.size()-1; i>= 0; i--) {
+            Tile snakePart = snakeBody.get(i);
+            if (i == 0) {
+                snakePart.x = snakeHead.x;
+                snakePart.y = snakeHead.y;
+            }
+            else {
+                Tile prevSnakePart = snakeBody.get(i-1);
+                snakePart.x = prevSnakePart.x;
+                snakePart.y = prevSnakePart.y;
+            }
+        }
+
         // Snake Head
         snakeHead.x += velocityX;
         snakeHead.y += velocityY;
