2fd4aea441f6987371e3e1402c3c086da4ed4bce
dorpen6 <alivalishov20@gmail.com>
2024-08-15 16:44:31 +0400

    Window Create

---
+++ src/App.java
@@ -0,0 +1,17 @@
+
+import javax.swing.JFrame;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 600;
+        int boardHeight = boardWidth;
+
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setSize(boardHeight, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
